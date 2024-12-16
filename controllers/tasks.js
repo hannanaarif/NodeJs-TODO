c4319f80dea1b92b0f80c3276b1e5c7ef41fc6aa
Aarif Hannan <hannanaarif@gmail.com>
2024-12-16 23:13:41 +0530

    added createTask & getTask logic

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,15 +1,28 @@
-
+const Task=require('../models/Task')
 
 const getAlltasks=(req,res)=>{
     res.send("All tasks from controller")
 }
 
-const  createTask=(req,res)=>{
-    res.json(req.body)
+const createTask= async(req,res)=>{
+    try {
+        const task=await Task.create(req.body)
+        res.status(201).json({task})
+    } catch (error) {
+        res.status(400).json('unable to create task',error.message)
+    }
 }
 
-const  getTask=(req,res)=>{    
-    res.json({id:req.params.id})
+const  getTask=async(req,res)=>{ 
+    try {
+        const data=await Task.findById(req.params.id)
+        if (!data) {
+            return res.status(404).json({ message: "Task not found" });
+        }
+        res.status(200).json({ message: "Found the task", data }); // Send data with success message
+    } catch (error) {
+        res.status(400).json({message:'unable to find task',error})
+    }   
 }
 
 const  updateTask=(req,res)=>{
